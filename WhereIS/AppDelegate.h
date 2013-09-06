4364f6bcdbcf948dd760007801bb0ace12d7881f
Francisco Rivera Uc <francisco@MacBook-Air-de-Francisco.local>
2013-09-05 23:19:00 -0500

    Initial Commit

---
+++ WhereIS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WhereIS
+//
+//  Created by Francisco Rivera Uc on 05/09/13.
+//  Copyright (c) 2013 Francisco Rivera Uc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
